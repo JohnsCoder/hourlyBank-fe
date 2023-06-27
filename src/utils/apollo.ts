68b8d6fb1af0b61056b8ca44108c661a54cb51da
JohnsCoder <joaopdossantos1515@gmail.com>
2023-06-27 18:53:58 -0300

    chore(apollo.ts): put api url on environment variables

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -2,6 +2,6 @@ import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client/cache";
 
 export const client = new ApolloClient({
-  uri: "https://hourlybank-be.onrender.com/",
+  uri: import.meta.env.VITE_URL_API,
   cache: new InMemoryCache({}),
 });
